2374fef88242a2a5a35bc67929fd53fe0d18c655
Horacy Miecierz <horekkeroh@gmail.com>
2026-01-09 15:21:39 +0100

    Karta cz1

---
+++ Assets/Scripts/Views/CardView.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class CardView : MonoBehaviour
+{
+   [serialized] private TMP_Text title;
+   [serialized] private TMP_Text description;
+   [serialized] private TMP_Text mana;
+   [serialized] private SpriteRenderer imageSR;
+   [serialized] private GameObject wrapper;
+
+}
